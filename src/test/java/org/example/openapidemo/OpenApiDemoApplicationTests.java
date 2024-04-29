b47718e74f959ebc2fe29b752b2561c0ddc67bac
Iqbal <muhammad.iqbal@moniepoint.com>
2024-04-29 19:24:52 +0500

    simple test

--- src/test/java/org/example/openapidemo/OpenApiDemoApplicationTests.java
+++ src/test/java/org/example/openapidemo/OpenApiDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package org.example.openapidemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OpenApiDemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void testListAllProducts() throws Exception {
+        mockMvc.perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.*", hasSize(1))) // Check if the array has items, assuming your test data setup has 1 product
+                .andExpect(jsonPath("$[0].name").value("Product 1")); // Check for specific product details
     }
 
 }

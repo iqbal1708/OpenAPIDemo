f0d4cb2e5d1b258ad61f2783ccbfb376ba02c9b3
Iqbal <muhammad.iqbal@moniepoint.com>
2024-04-29 20:38:39 +0500

    swagger ui

---
+++ src/main/java/org/example/openapidemo/controller/Test.java
@@ -0,0 +1,16 @@
+package org.example.openapidemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping
+    public String test() {
+        return "Hello World!";
+    }
+
+}
